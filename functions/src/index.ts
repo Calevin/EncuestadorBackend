9861a7acbbf3e62011cd30efc31e695097c7aece
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-01-11 03:28:31 -0300

    Ejemplo get a firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,14 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin  from 'firebase-admin';
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: ''
+});
+
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -8,4 +18,14 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.json({
       mensaje: "Hola Mundo desde Fuctions de Firebase!"
     });
+});
+
+export const encuesta = functions.https.onRequest( async (request, response) => {
+  functions.logger.info("getEncuesta", {structuredData: true});
+
+  const encuestaRef = db.collection('encuesta');
+  const docsSnap = await encuestaRef.get();
+  const resultadoEncuesta = docsSnap.docs.map( doc => doc.data() );
+
+  response.json( resultadoEncuesta );
 });
