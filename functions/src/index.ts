5959421c11ace7ec307ac9942e101ebe9eb422a1
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-01-11 03:55:58 -0300

    Update index.ts
     Borrado servicios get con functions (ejemplos)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,22 +15,6 @@ const db = admin.firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.json({
-      mensaje: "Hola Mundo desde Fuctions de Firebase!"
-    });
-});
-
-export const encuestaFunctions = functions.https.onRequest( async (request, response) => {
-  functions.logger.info("getEncuesta", {structuredData: true});
-
-  const encuestaRef = db.collection('encuesta');
-  const docsSnap = await encuestaRef.get();
-  const resultadoEncuesta = docsSnap.docs.map( doc => doc.data() );
-
-  response.json( resultadoEncuesta );
-});
 
 // Express
 const app = express();
