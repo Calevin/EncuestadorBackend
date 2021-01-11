b6ae6e94ce4764c180a537409d7671eb4c336893
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-01-11 04:11:08 -0300

    Update index.ts
    Cambios por recomendacion de estilo TSLint para deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,12 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as express from 'express';
-import * as cors from 'cors';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+import * as cors from "cors";
 
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: ''
 });
 
 const db = admin.firestore();
@@ -18,41 +17,39 @@ const db = admin.firestore();
 
 // Express
 const app = express();
-app.use( cors({ origin: true }) );
+app.use( cors({origin: true}) );
 
 // Servicio get /encuesta hecho con express
-app.get('/encuesta', async (req, res)  => {
-  const encuestaRef = db.collection('encuesta');
+app.get("/encuesta", async (req, res) => {
+  const encuestaRef = db.collection("encuesta");
   const docsSnap = await encuestaRef.get();
-  const resultadoEncuesta = docsSnap.docs.map( doc => doc.data() );
+  const resultadoEncuesta = docsSnap.docs.map( (doc) => doc.data() );
 
   res.json( resultadoEncuesta );
 });
 
-app.post('/encuesta/:id', async (req, res)  => {
-
+app.post("/encuesta/:id", async (req, res) => {
   const id = req.params.id;
-  const encuestaRef = db.collection('encuesta').doc( id );
+  const encuestaRef = db.collection("encuesta").doc( id );
   const docsSnap = await encuestaRef.get();
 
-  if( !docsSnap.exists ) {
+  if ( !docsSnap.exists ) {
     res.status(404).json({
       ok: false,
-      mensaje: 'No existe una opcion con ID: ' + id
+      mensaje: "No existe una opcion con ID: " + id,
     });
   } else {
-    const actual = docsSnap.data() || { votos: 0 };
+    const actual = docsSnap.data() || {votos: 0};
 
     await encuestaRef.update({
-      votos: actual.votos + 1
+      votos: actual.votos + 1,
     });
 
     res.json({
       ok: true,
-      mensaje: 'Opcion con ID: ' + id + ' votos actuales: ' + (actual.votos + 1)
+      mensaje: "Opcion ID: " + id + " votos actuales: " + (actual.votos + 1),
     });
   }
-
 });
 
 export const api = functions.https.onRequest( app );
