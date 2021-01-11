3d970e866a92387516e79f3f5d6d7fb34cbd95f5
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-01-11 03:53:01 -0300

    Update index.ts
     Servicio post para aumentar votos

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -45,4 +45,30 @@ app.get('/encuesta', async (req, res)  => {
   res.json( resultadoEncuesta );
 });
 
+app.post('/encuesta/:id', async (req, res)  => {
+
+  const id = req.params.id;
+  const encuestaRef = db.collection('encuesta').doc( id );
+  const docsSnap = await encuestaRef.get();
+
+  if( !docsSnap.exists ) {
+    res.status(404).json({
+      ok: false,
+      mensaje: 'No existe una opcion con ID: ' + id
+    });
+  } else {
+    const actual = docsSnap.data() || { votos: 0 };
+
+    await encuestaRef.update({
+      votos: actual.votos + 1
+    });
+
+    res.json({
+      ok: true,
+      mensaje: 'Opcion con ID: ' + id + ' votos actuales: ' + (actual.votos + 1)
+    });
+  }
+
+});
+
 export const api = functions.https.onRequest( app );
